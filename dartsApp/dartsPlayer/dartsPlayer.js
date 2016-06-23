f3c3b182cee762edc66cbebbdea82d824a55f4c4
Next <next@localhost.localdomain>
2016-06-23 15:55:57 +0200

    Added ngStorage for save the score of the rounds

--- dartsApp/dartsPlayer/dartsPlayer.js
+++ dartsApp/dartsPlayer/dartsPlayer.js
@@ -13,19 +13,25 @@ angular.module('dartsPlayer', [
 }])
 
 .controller('playerCtrl', ['$scope', '$localStorage', function ($scope, $localStorage) {
+	//Array che conterrà i vari oggetti giocatori
 	$scope.players = [];
 
+	//inizializzo e salvo i l'array di oggetti dei giocatori
+	localStorage.setItem('players', JSON.stringify($scope.players));
 	$scope.saved = localStorage.getItem('players');
-	if (localStorage.getItem('players')!==null) {
-		$scope.players = JSON.parse($scope.saved);
-	}
+	$scope.players = JSON.parse($scope.saved);
 
+	// Aggiunge il nome tramite l'input e inizializza il punteggio, creando un array di oggetti in player
 	$scope.addPlayer = function() {
-		$scope.players.push({ 'name':$scope.name });
+		$scope.players.push({ 
+			'name':$scope.name,
+			'totScore':501
+		});
     	$scope.name = '';
     	localStorage.setItem('players', JSON.stringify($scope.players));
 	}
 
+	//Cancella tutti i giocatori salvati
 	$scope.removePlayer = function() {
 		$scope.players = [];
 		localStorage.setItem('players', JSON.stringify($scope.players));
--- dartsApp/dartsRound/dartsRound.js
+++ dartsApp/dartsRound/dartsRound.js
@@ -1,6 +1,9 @@
 'use strict';
 
-angular.module('dartsRound', ['ngRoute'])
+angular.module('dartsRound', [
+	'ngRoute',
+	'ngStorage'
+])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/round', {
@@ -13,7 +16,107 @@ angular.module('dartsRound', ['ngRoute'])
 	$scope.saved = localStorage.getItem('players');
 	$scope.players = JSON.parse($scope.saved);
 
-	$scope.totPlayers = $scope.players.length;
+	
+	// Inizializzo il round a 1, e poi viene incrementato
+	$scope.rounds = 1;
 
+	// Calcolo il numero di giocatori che stanno giocando
+	$scope.totPlayers = $scope.players.length;
+	var i = $scope.totPlayers - 1;
+
+	// Inizializza la schermata con l'ultimo giocatore aggiunto
+	$scope.currentPlayer = $scope.players[i].name;
+
+	// Inizializzo il punteggio con quello dell'ultimo giocatore (perchè sarà il primo a giocare)
+	$scope.currentScore = $scope.players[i].totScore;
+
+	// Serve per fermare il gioco in caso di vittoria
+	$scope.win = false;
+
+	//viariabili che contengono il punteggio del round e il punteggio corrente di ogni lancio
+	$scope.scoreRound = 0;
+	var currentShot = 0;
+
+
+	$scope.loadRound = function() {
+		$scope.scoreRound = 0;
+		clearInput();
+		$scope.buttonClicked1 = false;
+		$scope.buttonClicked2 = false;
+		$scope.buttonClicked3 = false;
+		$scope.players[i].totScore = $scope.currentScore;
+		localStorage.setItem('players', JSON.stringify($scope.players));
+
+
+		if(i != 0) {
+			i--;
+			$scope.currentPlayer = $scope.players[i].name;
+			$scope.currentScore = $scope.players[i].totScore;
+		}
+		else{
+			$scope.rounds += 1;
+			i = $scope.totPlayers - 1;
+			$scope.currentPlayer = $scope.players[i].name;
+			$scope.currentScore = $scope.players[i].totScore;
+		}
+	}
+
+	$scope.addScore1 = function() {
+		currentShot = 0;
+		$scope.buttonClicked1 = true;
+		$scope.scoreRound += parseInt($scope.fShot);
+		currentShot = parseInt($scope.fShot);
+		$scope.currentScore -= currentShot;
+		if ($scope.currentScore == 0) {
+			$scope.win = true;
+			$scope.complimenti = "Complimenti sei il vincitore!!!";
+		}
+		if ($scope.currentScore < 0) {
+			$scope.currentScore += currentShot;
+			$scope.scoreRound = 0;
+		}
+
+	}
+
+	$scope.addScore2 = function() {
+		currentShot = 0;
+		$scope.buttonClicked2 = true;
+		$scope.scoreRound += parseInt($scope.sShot);
+		currentShot = parseInt($scope.sShot);
+		$scope.currentScore -= currentShot;
+		if ($scope.currentScore == 0) {
+			$scope.win = true;
+			$scope.complimenti = "Complimenti sei il vincitore!!!";
+		}
+		if ($scope.currentScore < 0) {
+			$scope.currentScore += currentShot;
+			$scope.scoreRound = 0;
+		}
+
+	}
+
+	$scope.addScore3 = function() {
+		currentShot = 0;
+		$scope.buttonClicked3 = true;
+		$scope.scoreRound += parseInt($scope.tShot);
+		currentShot = parseInt($scope.tShot);
+		$scope.currentScore -= currentShot;
+		if ($scope.currentScore == 0) {
+			$scope.win = true;
+			$scope.complimenti = "complimenti sei il vincitore!!!";
+		}
+		if ($scope.currentScore < 0) {
+			$scope.currentScore += currentShot;
+			$scope.scoreRound = 0;
+		}
+
+	} 
+
+	//Ripulisce le input dopo ogni click
+	var clearInput = function() {
+		$scope.fShot = '';
+		$scope.sShot = '';
+		$scope.tShot = '';
+	};
 
 }]);
