9a29c70804b94825e8cf6bae6c9d80f751e3c60c
Francesco Iubatti <francescoiubatti@MacBook-Air-di-Frank.local>
2016-07-18 10:54:58 +0200

    Modified player.spec file

--- app/player/player.spec.js
+++ app/player/player.spec.js
@@ -19,27 +19,3 @@ describe('Unit: players', function() {
 	}));
 
 });
-
-
-"use strict";
-
-describe('Players controller', function() {
-	var createCtrl;
-
-	beforeEach(module('app.player'));
-	beforeEach(inject(function($controller) {
-		createCtrl = function() {
-			return $controller('playerCtrl', {}, {});
-		};
-	}));
-
-	it('should have players property initialized to 0', function() {
-		var ctrl = createCtrl();
-		expect(ctrl.players.length).toBe(0);
-	});
-
-	it('should expose storagePlayers array', function() {
-		var ctrl = createCtrl();
-		expect(angular.isArray(ctrl.storagePlayers)).toBeTruthy();
-	});
-});
