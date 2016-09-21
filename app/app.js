cf9317bd77a7df87f6fa50828e6b04161fcad27d
Andrea Stronati <astronati@vendini.com>
2016-09-21 19:42:42 +0200

    Fixed indentation in the app.js file

--- app/app.js
+++ app/app.js
@@ -2,18 +2,18 @@
 
 // Declare app level module which depends on views, and components
 angular
-	.module('app', [
-		'ngRoute',
-		'ngStorage',
-		'app.home',
-		'app.player',
-		'app.round'
-	])
+  .module('app', [
+    'ngRoute',
+    'ngStorage',
+    'app.home',
+    'app.player',
+    'app.round'
+  ])
 
-	.config([
-		'$routeProvider',
-		function($routeProvider) {
-			// Redirects user to the main page if the selected one does not exist
-			$routeProvider.otherwise({redirectTo: '/'});
-		}
-	]);
+  .config([
+    '$routeProvider',
+    function($routeProvider) {
+      // Redirects user to the main page if the selected one does not exist
+      $routeProvider.otherwise({redirectTo: '/'});
+    }
+  ]);
