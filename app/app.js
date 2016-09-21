658e25977726320052b9aea06b21aac1146fe8a2
samulenti <samulenti@gmail.com>
2016-09-21 22:43:57 +0200

    Hope ready to code review

--- app/app.js
+++ app/app.js
@@ -1,19 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular
-  .module('app', [
-    'ngRoute',
-    'ngStorage',
-    'app.home',
-    'app.player',
-    'app.round'
-  ])
-
-  .config([
-    '$routeProvider',
-    function($routeProvider) {
-      // Redirects user to the main page if the selected one does not exist
-      $routeProvider.otherwise({redirectTo: '/'});
-    }
-  ]);
+angular.module('app', [
+	'ngRoute',
+	'ngStorage',
+	'app.home',
+	'app.player',
+	'app.round'
+])
+.config([
+	'$locationProvider',
+	'$routeProvider',
+	function($locationProvider, $routeProvider) {
+		$locationProvider.hashPrefix('!');
+		$routeProvider.otherwise({redirectTo: '/'});
+	}
+]);
--- app/player/player.controller.js
+++ app/player/player.controller.js
@@ -3,12 +3,15 @@
 angular.module('app.player')
 
 .controller('playerCtrl', ['$localStorage', function($localStorage) {
+	var addButtonClicked = false;
 	var vm = this;
 	//Array with all players ready to start the game
-	vm.players = [];
+	//vm.players = [];
+	
 	//Array with VIP players
 	vm.storagePlayers = [];
-
+	//Selected players
+	vm.selectedPlayers = [];
 	vm.savedStorage = $localStorage.storagePlayers = $localStorage.storagePlayers || [];
 
 	// load the VIP players in the table
@@ -18,36 +21,64 @@ angular.module('app.player')
 
 	// add player with a name, and a initial score
 	vm.addPlayer = function() {
-		vm.players.push({ 
+		
+		vm.storagePlayers.push({ 
 			'name':vm.name,
-			'totScore':501
+			'totScore':201,
+			'selected':false
 		});
-  	vm.name = ''; //clear input
-	  //save the last player
-		var currentPlayerSaved = {};
-	  currentPlayerSaved = vm.players[vm.players.length - 1]; 
-	  vm.storagePlayers.push(currentPlayerSaved); //add player in the VIP array
-	  $localStorage.players = vm.players;
-	  $localStorage.storagePlayers = vm.storagePlayers;
+		vm.name = ''; //clear input
+		//save the last player
+		//var currentPlayerSaved = {};
+		//currentPlayerSaved = vm.players[vm.players.length - 1]; 
+		//vm.storagePlayers.push(currentPlayerSaved); //add player in the VIP array
+		//$localStorage.players = vm.players;
+		$localStorage.storagePlayers = vm.storagePlayers;
+		addButtonClicked = false;
 	};
 
 	//remove all players in the list
-	vm.removeAllPlayers = function() {
+	/*vm.removeAllPlayers = function() {
 		vm.players = [];
 		$localStorage.players = vm.players;
-	};
+	};*/
 
 	// remove only the VIP player selected
-	vm.removePlayerStorage = function(friend) {
+	/*vm.removePlayerStorage = function(friend) {
 		var index = vm.storagePlayers.indexOf(friend);
 		vm.storagePlayers.splice(index, 1);
 		$localStorage.storagePlayers = vm.storagePlayers;
-	};
+	};*/
 
 	//take the selected VIP player from table and put it in the list of player
-	vm.addPlayerInList = function(friend) {
+	/*vm.addPlayerInList = function(friend) {
 		vm.buttonAddPlayerTable = true;
 		vm.players.push(friend);
 		$localStorage.players = vm.players;
+	};*/
+
+	vm.addPlayerClicked = function() {
+		addButtonClicked = true;
 	};
+
+	vm.isAddPlayerClicked = function() {
+		return addButtonClicked;  
+	};
+
+	vm.slectDeselectPlayer = function(friend) {
+		var index = vm.selectedPlayers.indexOf(friend);
+		if(index == -1)  //select
+			vm.selectedPlayers.push(friend);
+		else  //deselect
+			vm.selectedPlayers.splice(index, 1);
+		friend.selected=!friend.selected;
+		//$localStorage.selectedPlayers = vm.selectedPlayers;
+	};
+
+	vm.start = function() {
+	for(var i in vm.selectedPlayers)
+		vm.selectedPlayers[i].selected=false;
+	};
+
+
 }]);
