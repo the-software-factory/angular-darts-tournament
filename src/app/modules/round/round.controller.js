00da4e4e9336863176cb11ede9466d510d89c008
Andrea Stronati <astronati@vendini.com>
2016-10-02 20:39:10 +0200

    fixed #70 Several buttons are enabled if the GO button is enabled as well

--- src/app/modules/round/round.controller.js
+++ src/app/modules/round/round.controller.js
@@ -45,20 +45,47 @@ angular
        */
       vm.player = SelectedPlayers.getById($routeParams.playerID);
 
-      // TODO Add docblock
+      /**
+       * @ngdoc property
+       * @name RoundController#shotIndex
+       * @type {number}
+       * @propertyOf app.round.controller:RoundController
+       */
       vm.shotIndex = 0;
-  
-      // TODO Add docblock
+
+      /**
+       * @ngdoc property
+       * @name RoundController#round
+       * @type {number}
+       * @propertyOf app.round.controller:RoundController
+       */
       vm.round = $routeParams.roundID;
 
-      // TODO Add docblock
+      /**
+       * @ngdoc property
+       * @name RoundController#shots
+       * @type {Array}
+       * @propertyOf app.round.controller:RoundController
+       */
       vm.shots = [];
 
-      // TODO Add docblock
-      vm.button = null;
+      /**
+       * @ngdoc property
+       * @name RoundController#number
+       * @type {number}
+       * @propertyOf app.round.controller:RoundController
+       * @description The last pressed number
+       */
+      vm.number = null;
 
-      // TODO Add docblock
-      vm.blockCurrent = false;
+      /**
+       * @ngdoc property
+       * @name RoundController#boolean
+       * @type {boolean}
+       * @propertyOf app.round.controller:RoundController
+       * @description Determines if the last pressed number is disabled or not.
+       */
+      vm.isNumberDisabled = false;
 
       vm.init();
 
@@ -78,46 +105,62 @@ angular
         Match.setCurrentPlayer(vm.player);
       }
 
-      // TODO Add docblock
+      /**
+       * @ngdoc method
+       * @name RoundController#addPoint
+       * @kind function
+       * @methodOf app.round.controller:RoundController
+       * @description
+       * Stores the given point and blocks the button if it can no longer be clicked.
+       */
       function addPoint(value) {
-        vm.button = value;
+        vm.number = value;
         value = parseInt(value);
+
+        vm.shots[vm.shotIndex] = vm.shots[vm.shotIndex] ? vm.shots[vm.shotIndex] += value : value;
+
         switch (value) {
           case 0:
-            // TODO block current button
-            vm.blockCurrent = true;
+            vm.isNumberDisabled = true;
             break;
           case 25:
-            if (vm.shots[vm.shotIndex] == value) {
-              // TODO block current button
-              vm.blockCurrent = true;
+            // 25 has a double value that is 50 (the center)
+            if (vm.shots[vm.shotIndex] == value * 2) {
+              vm.isNumberDisabled = true;
             }
             break;
           default:
-            if (vm.shots[vm.shotIndex] == value * 2) {
-              // TODO block current button
-              vm.blockCurrent = true;
+            // Any other number has a double (the border of the dartboard) or a triple value
+            if (vm.shots[vm.shotIndex] == value * 3) {
+              vm.isNumberDisabled = true;
             }
             break;
         }
-        vm.shots[vm.shotIndex] = vm.shots[vm.shotIndex] ? vm.shots[vm.shotIndex] += value : value;
-        // TODO block all other buttons
       }
 
       // TODO Add docblock
       function cancel() {
+        // TODO To implement
         alert('to implement');
       }
 
-      // TODO Add docblock
+      /**
+       * @ngdoc method
+       * @name RoundController#isRoundCompleted
+       * @kind function
+       * @methodOf app.round.controller:RoundController
+       * @return {boolean} True if the round is completed.
+       * @description
+       * Determines if the round is completed.
+       */
       function isRoundCompleted() {
-        return vm.shots.length == 3 && vm.button == null;
+        return vm.shots.length == 3 && vm.number == null;
       }
 
       // TODO Add docblock
       function confirm() {
-        vm.blockCurrent = false;
-        vm.button = null;
+        vm.isNumberDisabled = false;
+        vm.number = null;
         Match.addRound(vm.player, vm.round, vm.getRoundSum());
         if (vm.shots.length === 3) {
           return;
@@ -126,13 +169,32 @@ angular
         // TODO Enable all buttons
       }
 
-      // TODO Add docblock
+      /**
+       * @ngdoc method
+       * @name RoundController#getMissingPoints
+       * @kind function
+       * @methodOf app.round.controller:RoundController
+       * @return {number} The missing points to win.
+       * @description
+       * Determines the missing points taking into account the ones done in the current round.
+       */
       function getMissingPoints() {
-        // FIXME It should calc the made points.
-        return Match.getInitialPoints() - Match.getPoints(vm.player, vm.round - 1) - vm.getRoundSum();
+        var missingPoints = Match.getInitialPoints() - vm.getRoundSum();
+        if (vm.round > 1) {
+          missingPoints -= Match.getPoints(vm.player, vm.round - 1);
+        }
+        return missingPoints;
       }
 
-      // TODO Add docblock
+      /**
+       * @ngdoc method
+       * @name RoundController#getRoundSum
+       * @kind function
+       * @methodOf app.round.controller:RoundController
+       * @return {number} The sum of points of the current round
+       * @description
+       * Returns the sum of points of the current round.
+       */
       function getRoundSum() {
         var sum = 0;
         angular.forEach(vm.shots, function(value) {
@@ -148,9 +210,20 @@ angular
         $location.path('summary/round/' + nextRound + '/player/' + Match.getNextPlayer().id);
       }
 
-      // TODO Add docblock
+      /**
+       * @ngdoc method
+       * @name RoundController#isButtonDisabled
+       * @kind function
+       * @methodOf app.round.controller:RoundController
+       * @return {boolean} True if the button is disabled.
+       * @description
+       * Determines if the given button is disabled or not.
+       */
       function isButtonDisabled(value) {
-        return vm.button != null && value != vm.button || (vm.blockCurrent && value == vm.button);
+        return (value != vm.number && vm.number != null) || // The button has not be pressed
+          (vm.isNumberDisabled && value == vm.number) || // The given value is the one of the pressed button and it can
+          // no longer be pressed
+          vm.isRoundCompleted(); // Round is completed so any button will be disabled
       }
 
     }
